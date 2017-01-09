c25be4c9cb47e2fa6620666db1687e13a6948f5d
Shoichi Sakane <github-shoichi@tanu.org>
2017-01-09 09:46:34 +0900

    important variables are moved into the top.

--- sample-test.sh
+++ sample-test.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
+export CAF_APP_PATH="./"
+export CAF_APP_CONFIG_FILE="package_config.ini"
+
 export CAF_APP_PERSISTENT_DIR="/data"
 export CAF_APP_LOG_DIR="/log"
-export CAF_APP_CONFIG_FILE="package_config.ini"
 export CAF_APP_CONFIG_DIR="/config"
 export CAF_APP_USERNAME="username"
 export CAF_HOME="caf"
 export CAF_HOME_ABS_PATH="/home/caf"
-export CAF_APP_PATH="./"
 export CAF_MODULES_PATH="/module_path"
 export CAF_APP_DIR="/app_dir"
 export CAF_MODULES_DIR="/module_dir"
